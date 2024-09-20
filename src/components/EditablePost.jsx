44b2ed1ad655ec2effaa17433782c0c6fe27abe7
hsc996 <han.scaife@gmail.com>
2024-09-20 15:51:45 +1000

    feat: component structures started

---
+++ src/components/EditablePost.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default class EditablePost extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className='editablePostContainer'>
+
+            </div>
+        )
+    }
+}
